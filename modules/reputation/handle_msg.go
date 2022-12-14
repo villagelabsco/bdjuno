d4bad46fd7e415e915df7104ceb8551883886372
Boris Lachev <boris.lachev@icloud.com>
2022-12-14 15:01:22 +0200

    Handle post reputation msg feedback

--- modules/reputation/handle_msg.go
+++ modules/reputation/handle_msg.go
@@ -18,14 +18,61 @@ package reputation
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/forbole/bdjuno/v3/database/types"
 	juno "github.com/forbole/juno/v3/types"
+	"github.com/pkg/errors"
 	reputationtypes "github.com/villagelabs/villaged/x/reputation/types"
+	"time"
 )
 
 func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
+	if len(tx.Logs) == 0 {
+		return nil
+	}
+
+	switch cosmosMsg := msg.(type) {
+	case *reputationtypes.MsgPostFeedback:
+		return m.HandleMsgPostFeedback(cosmosMsg)
+	}
+
 	return nil
 }
 
-func (m *Module) handleMsgPostFeedback(height int64, msg *reputationtypes.MsgPostFeedback) error {
-	return nil
+func (m *Module) HandleMsgPostFeedback(msg *reputationtypes.MsgPostFeedback) error {
+	err := m.db.SavePostFeedback(msg)
+	if err != nil {
+		return errors.Wrap(err, "error while saving reputation post feedback")
+	}
+
+	fbItem := &types.ReputationFeedbackItem{
+		CreatorAcc: msg.Creator,
+		DestAcc:    msg.DstAccount,
+		TxId:       msg.TxId,
+		Ref:        msg.Ref,
+		// TODO: Any way to get a block time?
+		Timestamp: time.Now(),
+	}
+
+	fb, err := m.db.FeedbackAggregate(msg.DstAccount)
+	if err != nil {
+		return errors.Wrap(err, "error while getting reputation feedback aggregate")
+	}
+
+	switch reputationtypes.FeedbackType(msg.FbType) {
+	case reputationtypes.PositiveFeedback:
+		fb.CptPositive += 1
+		fb.Positive = append(fb.Positive, fbItem.ToDto())
+	case reputationtypes.NeutralFeedback:
+		fb.CptNeutral += 1
+		fb.Neutral = append(fb.Neutral, fbItem.ToDto())
+	case reputationtypes.NegativeFeedback:
+		fb.CptNegative += 1
+		fb.Negative = append(fb.Negative, fbItem.ToDto())
+	}
+
+	if fb.Index == "" {
+		return m.db.InsertFeedbackAggregate(fb)
+	} else {
+		return m.db.UpdateFeedbackAggregate(fb)
+	}
 }
