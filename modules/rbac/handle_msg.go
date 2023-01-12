bcfa792a01b2d2946a826b56b1c36d4ad2f46a01
Boris Lachev <boris.lachev@icloud.com>
2023-01-12 17:11:02 +0200

    Add RBAC handle msg

--- modules/rbac/handle_msg.go
+++ modules/rbac/handle_msg.go
@@ -17,11 +17,45 @@
 package rbac
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	juno "github.com/forbole/juno/v3/types"
+	rbactypes "github.com/villagelabs/villaged/x/rbac/types"
 )
 
 func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
-	//TODO implement me
-	panic("implement me")
+	switch cosmosMsg := msg.(type) {
+	case *rbactypes.MsgDeclareRole:
+		return m.HandleMsgDeclareRole(tx.Height, cosmosMsg)
+	case *rbactypes.MsgUpdateGroupMembers:
+		return m.HandleMsgUpdateGroupMembers(tx.Height, cosmosMsg)
+	case *rbactypes.MsgTransferRoleOwnership:
+		return m.HandleMsgTransferRoleOwnership(tx.Height, cosmosMsg)
+	case *rbactypes.MsgUpdateRole:
+		return m.HandleMsgUpdateRole(tx.Height, cosmosMsg)
+	case *rbactypes.MsgSetRoleDelegates:
+		return m.HandleMsgSetRoleDelegates(tx.Height, cosmosMsg)
+	default:
+		return fmt.Errorf("unrecognized rbac message type: %T", msg)
+	}
+}
+
+func (m *Module) HandleMsgDeclareRole(height int64, msg *rbactypes.MsgDeclareRole) error {
+	return nil
+}
+
+func (m *Module) HandleMsgUpdateGroupMembers(height int64, msg *rbactypes.MsgUpdateGroupMembers) error {
+	return nil
+}
+
+func (m *Module) HandleMsgTransferRoleOwnership(height int64, msg *rbactypes.MsgTransferRoleOwnership) error {
+	return nil
+}
+
+func (m *Module) HandleMsgUpdateRole(height int64, msg *rbactypes.MsgUpdateRole) error {
+	return nil
+}
+
+func (m *Module) HandleMsgSetRoleDelegates(height int64, msg *rbactypes.MsgSetRoleDelegates) error {
+	return nil
 }
